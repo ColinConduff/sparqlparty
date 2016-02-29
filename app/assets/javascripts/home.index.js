4f24ca0e395853d7e467c3c63705587d93ad1800
Colin Conduff <cconduff098@gmail.com>
2016-02-29 13:56:27 -0600

    Spatial relationship functionality fully implemented with all operations available

--- app/assets/javascripts/home.index.js
+++ app/assets/javascripts/home.index.js
@@ -160,8 +160,6 @@ function baseQueryRequest(query, ifSuccessfulDoThis)
 {
     var endpoint = "http://geoquery.cs.jmu.edu:8081/parliament/sparql";
 
-    console.log(query);
-
     var request = $.ajax({
         type: "GET",
         url: endpoint,
@@ -182,7 +180,7 @@ function baseQueryRequest(query, ifSuccessfulDoThis)
     });
 }
 
-function drawSpatialRelationshipLayerWithManyToOne(feature1Type, feature1Relationship, feature1SearchTerm, feature2, operation)
+function getSpatialRelationshipResultsOfManyToOne(feature1Type, feature1Relationship, feature1SearchTerm, feature2, operation)
 {
     var query = 'PREFIX geo: <http://www.opengis.net/ont/geosparql#> PREFIX geof: <http://www.opengis.net/def/function/geosparql/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX units: <http://www.opengis.net/def/uom/OGC/1.0/> SELECT DISTINCT ?feature ?label WHERE { ?feature rdf:type <' + feature1Type + '> . ?feature rdfs:label ?label . ?feature <' + feature1Relationship +'> ?obj1 . FILTER( regex(str(?obj1), "' + feature1SearchTerm + '", "i" ) ) . ?feature geo:hasGeometry ?g1 . ?g1 geo:asWKT ?wkt1 . <' + feature2 + '> geo:hasGeometry ?g2 . ?g2 geo:asWKT ?wktf2 . BIND(?wktf2 AS ?wkt2) . FILTER (geof:' + operation + '(?wkt1, ?wkt2)) .}';
     var willDoThisUponSuccessfulQuery = function(msg) { 
@@ -192,9 +190,19 @@ function drawSpatialRelationshipLayerWithManyToOne(feature1Type, feature1Relatio
     baseQueryRequest(query, willDoThisUponSuccessfulQuery);
 }
 
-function drawSpatialRelationshipLayerWith2Features(feature1, feature2, operation)
+function getSpatialRelationshipResultsOf2Features(feature1, feature2, operation)
 {
     var query = 'PREFIX geo: <http://www.opengis.net/ont/geosparql#> PREFIX geof: <http://www.opengis.net/def/function/geosparql/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX units: <http://www.opengis.net/def/uom/OGC/1.0/> SELECT DISTINCT ?feature ?label WHERE { <' + feature1 + '> geo:hasGeometry ?g1 . ?g1 geo:asWKT ?wktf1 . BIND(?wktf1 AS ?wkt1) . <' + feature2 + '> geo:hasGeometry ?g2 . ?g2 geo:asWKT ?wktf2 . BIND(?wktf2 AS ?wkt2) . FILTER (geof:' + operation + '(?wkt1, ?wkt2)) .}';
+    var willDoThisUponSuccessfulQuery = function(msg) { 
+        updateTable(msg, 'spatialRelResultsTable');
+    }
+
+    baseQueryRequest(query, willDoThisUponSuccessfulQuery);
+}
+
+function getSpatialRelationshipResultsOfManyToMany(feature1Type, feature1Relationship, feature1SearchTerm, feature2Type, feature2Relationship, feature2SearchTerm, operation)
+{
+    var query = 'PREFIX geo: <http://www.opengis.net/ont/geosparql#> PREFIX geof: <http://www.opengis.net/def/function/geosparql/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX units: <http://www.opengis.net/def/uom/OGC/1.0/> SELECT DISTINCT ?feature ?label WHERE { ?feature rdf:type <' + feature1Type + '> . ?feature rdfs:label ?label . ?feature <' + feature1Relationship +'> ?obj1 . FILTER( regex(str(?obj1), "' + feature1SearchTerm + '", "i" ) ) . ?feature geo:hasGeometry ?g1 . ?g1 geo:asWKT ?wkt1 . ?feature2 rdf:type <' + feature2Type + '> . ?feature2 rdfs:label ?label2 . ?feature2 <' + feature2Relationship +'> ?obj2 . FILTER( regex(str(?obj2), "' + feature2SearchTerm + '", "i" ) ) . ?feature2 geo:hasGeometry ?g2 . ?g2 geo:asWKT ?wkt2 . FILTER (geof:' + operation + '(?wkt1, ?wkt2)) .}';
     var willDoThisUponSuccessfulQuery = function(msg) { 
         updateTable(msg, 'spatialRelResultsTable');
     }
